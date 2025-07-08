66f12c79eee37db9bbd9542b04df4e474b2a76c3
Thomas Chuna <37314207+chunatho@users.noreply.github.com>
2025-07-08 14:41:34 +0200

    ensure arrays are c ordered for numba

--- src/C2KBryan.py
+++ src/C2KBryan.py
@@ -19,6 +19,8 @@ def C2KBryan_solve(A, b, C, mu, \
     # Eur. Biophys. J. 1990, 18, 165-174, doi:10.1007/BF02427376.
     # Then this program uses the chi2kin algorithm to select a single best alpha value
     # and returns the solution at that alpha value.
+    # Kaufmann, J., and Karsten H.. "ana_cont: Python package for analytic continuation."
+    # Computer Physics Communications 282 (2023): 108519. doi:10.1016/j.cpc.2022.108519
     # Input:
     #  A: matrix to be inverted.
     #  b: data
@@ -91,35 +93,38 @@ def np_all(x):
     return out
 
 @njit
-def Bryans_alg(A, b, C, mu, alpha: float64, cond_upperbound,  max_iter, max_fail):
-    # Bryan's Algorithm published - https://link.springer.com/article/10.1007/BF02427376
-    # This code follows the convention of Asakawa et al. https://arxiv.org/abs/hep-lat/0011040
-    # Input:
-    #   A: matrix to be inverted.
-    #   b: data
-    #   C: data's convariance matrix.
-    #   mu: default model or prior.
-    #   alpha: weight of the entropic prior.
-    #   cond_upperbound: Cut off for maximum condition number of discretized kernel.
-    #   max_iter: maximum number of Levenberg Marquart steps to take
-    #   max_fail: maximum number of # of adjustments made to the LM parameter in an iteration
-    # Output:
-    #     x: solution
-
-    [Ntau,_] = shape(A)
+def Bryans_alg(A, b, C, mu, alpha, cond_upperbound,  max_iter, max_fail):
+    """
+    Bryan's Algorithm published - https://link.springer.com/article/10.1007/BF02427376
+    This code follows the convention of Asakawa et al. https://arxiv.org/abs/hep-lat/0011040
+    Input:
+      A: matrix to be inverted.
+      b: data
+      C: data's convariance matrix.
+      mu: default model or prior.
+      alpha: weight of the entropic prior.
+      cond_upperbound: Cut off for maximum condition number of discretized kernel. 
+         default (1e10) - this default value is set in the MEM Bayesian averaging wrapper function.
+      max_iter: maximum number of Levenberg Marquart steps to take
+         default (500) - this default value is set in the MEM Bayesian averaging wrapper function.
+      max_fail: maximum number of # of adjustments made to the LM parameter in an iteration
+         default (2000) - this default value is set in the MEM Bayesian averaging wrapper function.
+    Output:
+        x: solution
+    """
+    [Ntau,Nomega] = shape(A)
     # Decompose the convariance matrix
     eigvals, R = eigh(C) # C = R Lambda R.T (note Cinv = R lambda^{-1} R.T )
     eigvals_inv_matrix = diag(1./eigvals)
 
     # Rotate into diagonal covariance space (eqn 3.39)
     Arot = copy(R.T @ A)
-    brot = copy(R.T @ b)
-    #brot = copy(R.T @ reshape(b, (Ntau,1)) )
-
-    # let svd produce A = V@diag(S)@Uh, thus we have to transpose Vh to get V
-    # A=V@S@U.T disagrees with the typical A=U@S@V.T, but it is
+    brot = copy(R.T @ reshape(b, (Ntau,1)) )
+    
+    # let svd produce A = V@diag(S)@Uh, thus we have to transpose Vh to get V 
+    # A=V@S@U.T disagrees with the typical A=U@S@V.T, but it is 
     # how Asakawa writes it (see the text below eqn 3.30).
-    V,S,Uh = svd(Arot); U=Uh.T
+    V0,S,Uh = svd(Arot); U0=Uh.T
     # pre-condition the inversion to a set condition number
     if (cond_upperbound < 0.0):
         r=Ntau;
@@ -130,28 +135,28 @@ def Bryans_alg(A, b, C, mu, alpha: float64, cond_upperbound,  max_iter, max_fail
                 r=i+1
             else:
                 break
-    V=V[:,0:r]; U=U[:,0:r]; S = diag(S[0:r])
-    #DDL = eigvals_inv_matrix
+    # use copy to make sure arrays are c ordered for numba
+    V=copy(V0[:,:r:1]); U=copy(U0[:,:r:1]); S = diag(S[:r:1])
+
     # Precompute the M for computational savings (eqn 3.37)
     M = S @ V.T @ (eigvals_inv_matrix) @ V @ S
 
-    #y = ones((r,1), dtype='float64')
-    y = zeros((r,1), dtype='float64')
+    y = zeros((r,1), dtype='float64') # start optimization at default model
     err_list=[ chi_sq(Arot, brot, eigvals_inv_matrix, mu, U, y) ];
-
+    
     #initialize algorithm constants
-    up=2.0; c0=0.1; i=0;
+    up=2.; c0=.1; i=0;
     while (i < max_iter):
         i+=1 # this index keeps track of the number of update iteration
         lam=1e-3; # this is the Levenberg-Marquardt (LM) parameter, it's like a learning rate.
         fail=0; # this index tracks the # of adjustments made to the LM parameter this iteration
-
+        
         # ------------------------------------------------------- #
         # Compute RHS of eqn 3.36, where b has been relabeled `y' #
         # ------------------------------------------------------- #
-        x = mu*exp(U @ y) # eqn (3.29) x = m * exp(a) & eqn (3.33) a = Uy
-        DL = eigvals_inv_matrix @ ( Arot @ x - brot ) # derivative of Likelihood (L) w.r.t. A@x
-        g = S @ V.T @ DL # compute g from 3.34
+        x = mu*exp(U @ y) # eqn (3.29) x = m * exp(a) & eqn (3.33) a = Uy 
+        DL = eigvals_inv_matrix @ ( Arot @ x - brot ) # derivative of Likelihood (L) w.r.t. Arot@x
+        g = S @ V.T @ DL # compute g from 3.34 
         RHS = -alpha * y - g # compute the RHS of eqn 3.36
 
         # -------------------------------------------------------------- #
@@ -159,34 +164,33 @@ def Bryans_alg(A, b, C, mu, alpha: float64, cond_upperbound,  max_iter, max_fail
         # until an acceptable update is found of max_fail is reached.    #
         # -------------------------------------------------------------- #
         T = U.T @ diag(x.flatten()) @ U # pre-compute T from eqn 3.37
-        while ( fail < max_fail ):
+        while ( fail < max_fail ): 
             LHS = (alpha + lam) * eye(r) + M @ T # compute the LHS of eqn 3.36
             delta = solve(LHS,RHS) # leave it up to numpy on how to solve it
             new_y = y + delta
             err = chi_sq(Arot, brot, eigvals_inv_matrix, mu, U, new_y)
+            #flag = ( (delta.T @ T @ delta)[0,0] > c0 ) # eqn 3.38
             if ( isnan(err) or err_list[-1] < err): # only update if Chi_sq improves!
-            #if ( (delta.T @ T @ delta)[0,0] > c0*sum(mu) ): # Asakawa update
+            #if ( (delta.T @ T @ delta)[0,0] > c0*sum(m) ): # Asakawa update
                 lam*=up;
                 fail+=1;
             else: # successful update - reset Lev. Marq. parameter
                 lam=1e-3
                 break
-
+                
         # check if fail limit has been reached
-        if (fail == max_fail):
-            print(' --- reached max fail! -- ')
-            print('round', i, 'max iter', max_iter, 'abs(err_list[-2] - err) / err < 1e-5', abs(err_list[-2] - err) / err)
+        if (fail == max_fail): 
+            print("reached max fail")
             break
         #update and check for convergence
-        else:
+        else:  
             err_list.append(err);
             y=new_y;
             if ( abs(err_list[-2] - err) / err < 1e-5 ):
                 break
-
-    #print('round', i, 'max iter', max_iter, 'abs(err_list[-2] - err) / err < 1e-5', abs(err_list[-2] - err) / err)
-    return mu*exp(U @ y); # eqn (3.29) x = m * exp(a) & eqn (3.33) a = Uy
-
+    
+    x = mu*exp(U @ y); # eqn (3.29) x = m * exp(a) & eqn (3.33) a = Uy
+    return x
 
 @njit
 def chi_sq(A, b, eigvals_inv_matrix, mu, U, y):
